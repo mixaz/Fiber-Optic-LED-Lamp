a00af495e73aed6a6ac924edfbb52e24e8de6d5f
Mikhail Zemlyanukha <gmixaz@gmail.com>
2022-01-24 00:58:34 +0300

    added 2 buttons for brightness

--- Fiber_Optic_LED_Lamp.ino
+++ Fiber_Optic_LED_Lamp.ino
@@ -6,13 +6,14 @@
 //#endif
 
 // define the LEDs
-#define LED_PIN 8          // pin the LEDs are connected to
-#define BTN_PIN 7          // Push button on this pin [3]
+#define BTN_PIN           7          // Push button on this pin [3]
+#define LED_PIN           8          // pin the LEDs are connected to
+#define BRIGTH_PLUS_PIN   9
+#define BRIGTH_MINUS_PIN  10
 
-#define ANALOG_MAX  (1024*1)
+#define ANALOG_MAX  (1024*1)        // (1024*4) for STM32
 
 #define NUM_LEDS 32
-#define BRIGHTNESS 160 //maximum brightness
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 #define UPDATES_PER_SECOND 100
@@ -26,8 +27,12 @@ CRGB leds[NUM_LEDS];
 #include "uno_vu_line.h"
 
 PinButton FunctionButton(BTN_PIN,INPUT_PULLUP);
+PinButton brightPlusButton(BRIGTH_PLUS_PIN,INPUT_PULLUP);
+PinButton brightMinusButton(BRIGTH_MINUS_PIN,INPUT_PULLUP);
 
-int setMode = 0;
+static int setMode = 0;
+
+static int brightness = 200;
 
 //#ifdef ARDUINO_ARCH_AVR
 //void wakeUp(){
@@ -46,23 +51,29 @@ int setMode = 0;
 //#endif
 
 void setup() {
-	delay(1000); // power-up safety delay
-	Serial.begin(115200);
+  delay(1000); // power-up safety delay
+  
+  Serial.begin(115200);
+  Serial.println("fastled started");
+  
   pinMode(LEFT_IN_PIN, INPUT_PULLUP);
-	FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setMaxPowerInVoltsAndMilliamps(5, MAX_MILLIAMPS);
-	FastLED.setBrightness(BRIGHTNESS);
-	FastLED.clear();
-	Serial.println("fastled started");
-//	EEPROM.update(0, setMode);        // at first run uncomment this for lines for initializing the eeprom data
-//	EEPROM.update(1, colorCounter);
-//	EEPROM.update(2, paletteCounter);
-//	EEPROM.update(3, gCurrentPatternNumber);
+  FastLED.setBrightness(brightness);
+  FastLED.clear();
 
-//	setMode = EEPROM.read(0);
-//	colorCounter = EEPROM.read(1);
-//	paletteCounter = EEPROM.read(2);
-//	gCurrentPatternNumber = EEPROM.read(3);
+  colorCounter = 1;
+  
+//  EEPROM.update(0, setMode);        // at first run uncomment this for lines for initializing the eeprom data
+//  EEPROM.update(1, colorCounter);
+//  EEPROM.update(2, paletteCounter);
+//  EEPROM.update(3, gCurrentPatternNumber);
+
+//  setMode = EEPROM.read(0);
+//  colorCounter = EEPROM.read(1);
+//  paletteCounter = EEPROM.read(2);
+//  gCurrentPatternNumber = EEPROM.read(3);
 }
 
 void fiberLedShow(void) {
@@ -71,37 +82,31 @@ void fiberLedShow(void) {
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
 
-int vvv = 0;
-
 void loop() {
 
-  int vv = digitalRead(BTN_PIN);
-  if(vv != vvv) {
-    vvv = vv;
-    Serial.print(vv);
-  }
+  FunctionButton.update();
+  brightPlusButton.update();
+  brightMinusButton.update();
 
-	FunctionButton.update();
-
-	if (FunctionButton.isSingleClick()) {
+  if (FunctionButton.isSingleClick()) {
     Serial.println("single click");
-		if (setMode == 0) {
-			colorCounter++;
-			if (colorCounter > 17) {
-				colorCounter = 0;
-			}
+    if (setMode == 0) {
+      colorCounter++;
+      if (colorCounter > 17) {
+        colorCounter = 0;
+      }
      Serial.print("colorCounter: "); Serial.println(colorCounter);
-		}
-		else if (setMode == 1) {
-			paletteCounter++;
-			if (paletteCounter > 11) { // adjust if you have more or less than 34 palettes
-				paletteCounter = 0;
-			}
+    }
+    else if (setMode == 1) {
+      paletteCounter++;
+      if (paletteCounter > 11) { // adjust if you have more or less than 34 palettes
+        paletteCounter = 0;
+      }
      Serial.print("paletteCounter: "); Serial.println(paletteCounter);
-		}
-		else if (setMode == 2) {
-			nextPattern();  // Change to the next pattern
-		}
+    }
+    else if (setMode == 2) {
+      nextPattern();  // Change to the next pattern
+    }
     else if (setMode == 3) {
         Serial.print("buttonPushCounter: ");
         Serial.println(buttonPushCounter);
@@ -110,49 +115,69 @@ void loop() {
     else if (setMode == 4) {
         Serial.println("Single click is not used in this mode");
     }
-	}
-	else if (FunctionButton.isDoubleClick()) {
+  }
+  else if (FunctionButton.isDoubleClick()) {
     Serial.println("double click");
-		setMode++;
-		if (setMode > 4) {
-			setMode = 0;
-		}
+    setMode++;
+    if (setMode > 4) {
+      setMode = 0;
+    }
     Serial.print("double click, setMode: ");
     Serial.println(setMode);
-	}
-	else if (FunctionButton.isLongClick()) {
+  }
+  else if (FunctionButton.isLongClick()) {
     Serial.println("long click");
-		FastLED.clear();
-		FastLED.show();
-//		EEPROM.update(0, setMode);
-//		EEPROM.update(1, colorCounter);
-//		EEPROM.update(2, paletteCounter);
-//		EEPROM.update(3, gCurrentPatternNumber);
-//		Serial.println("shooting down");
-//		delay(500);
-//		Going_To_Sleep();
-//		Serial.println("waking up");
-	}
+    FastLED.clear();
+    FastLED.show();
+//    EEPROM.update(0, setMode);
+//    EEPROM.update(1, colorCounter);
+//    EEPROM.update(2, paletteCounter);
+//    EEPROM.update(3, gCurrentPatternNumber);
+//    Serial.println("shooting down");
+//    delay(500);
+//    Going_To_Sleep();
+//    Serial.println("waking up");
+  }
 
-	if (setMode == 0) {
-		if (colorCounter % 2 == 0) {
-  		float breath = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0;
-  		FastLED.setBrightness(breath);
-		}
-		else {
-			FastLED.setBrightness(BRIGHTNESS);
-		}
-		ChangeColorPeriodically();
+  int newBrightness = brightness;
+  if (brightPlusButton.isSingleClick()) { 
+    newBrightness += 5; 
+  }   
+  else if (brightPlusButton.isLongClick()) { 
+    newBrightness += 20; 
+  }   
+  if (brightMinusButton.isSingleClick()) { 
+    newBrightness -= 5; 
+  }   
+  else if (brightMinusButton.isLongClick()) { 
+    newBrightness -= 20; 
+  }   
+  if (newBrightness != brightness) { 
+    newBrightness = constrain(newBrightness, 0, 254); 
+    Serial.print("brightness: ");
+    Serial.println(newBrightness);
+    brightness = newBrightness;
+  }   
+
+  if (setMode == 0) {
+    if (colorCounter % 2 == 0) {
+      float breath = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0/255*brightness;
+      FastLED.setBrightness(breath);
+    }
+    else {
+      FastLED.setBrightness(brightness);
+    }
+    ChangeColorPeriodically();
     fiberLedShow();
-	}
-	else if (setMode == 1) {
-		FastLED.setBrightness(BRIGHTNESS);
-		ChangePalettePeriodically();
-		static uint8_t startIndex = 0;
-		startIndex = startIndex + 1;
-		FillLEDsFromPaletteColors(startIndex);
+  }
+  else if (setMode == 1) {
+    FastLED.setBrightness(brightness);
+    ChangePalettePeriodically();
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1;
+    FillLEDsFromPaletteColors(startIndex);
     fiberLedShow();
-	}
+  }
   else if (setMode == 2) {
     gPatterns[gCurrentPatternNumber]();
     fiberLedShow();
@@ -170,4 +195,5 @@ void loop() {
     runSelectedPattern();
   }
 
+  delay(50); 
 }
